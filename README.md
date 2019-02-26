a3b26017f144ab8287c4e3cd9d200ef0213a7965
tony <tony@gromit130-179.hampshire.edu>
2019-02-26 12:52:11 -0500

    added gif and resize the canvas

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Clique_game
 
+Clique_game is a clique searching game. It is designed to demonstrate NP-complete problems.
+
+## User Stories
 - [x] It can generate a random graph based on the number user passed in.
+- [ ] Search clique for given size x.
+
+## Notes
+- Need to have a way to convert the visual graph into a graph data structure.
+
+## Gif Walkthrough
+![walkthrough](alpha_graph_generator)
+
+
--- graph.js
+++ graph.js
@@ -1,13 +1,14 @@
 var numNodes = document.getElementById('Nodes');
 var form = document.getElementById('ready');
-var canvas = document.getElementById('canvas');
+var canvas = document.getElementById('myCanvas');
 
 
 
 form.addEventListener('submit', function(event){
     // Generates a random graph with six nodes and
     // an edge between each node is created with a probability of 0.3
-    var G = jsnx.binomialGraph(parseFloat(numNodes.value), 0.6);
+    var G = jsnx.binomialGraph(parseFloat(numNodes.value), 0.1);
+    
     jsnx.draw(G, {
       element: canvas,  
       weighted: true,
